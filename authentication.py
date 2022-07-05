dda612f2d7d7d4300c03b980a61cf56978767e64
levbush <lev.o.bushuev@gmail.com>
2022-07-05 21:21:42 +0300

    updated

--- authentication.py
+++ authentication.py
@@ -3,6 +3,19 @@ name = input()
 name = name + '.'
 rank = 0
 action = ''
+secsim = []
+numbers = []
+num_in_secsim = None
+for i in range(1, 34):
+    if i < 10:
+        numbers.append('0' + str(i))
+    else:
+        numbers.append(str(i))
+digits = [j for j in range(10)]
+letters = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+text_encrypted = ''
+text_num = ''
+text_decrypted = ''
 print('Введите пароль')
 password = input()
 if password == 'empee':
@@ -18,22 +31,22 @@ else:
     print('Здравствуйте,', name, 'Вы ошиблись, либо вас сдесь не должно быть')
 if rank != 0:
     while action != 'Выход':
-        print('Введите название действия, которое вы хотите сделать. Возможные действия: "Шифр1", "Шифр2", "Выход"')
+        print('Введите название действия, которое вы хотите сделать. Возможные действия: "Шифр", "Ввод secsim", "Выход"')
         action = input()
-        if action == 'Шифр1':
+        if action == 'Ввод secsim':
+            if rank == 3:
+                for _ in range(10):
+                    secsim.append(input())
+            else:
+                print('Вам не разрешено выполнять это действие')
+        if action == 'Шифр':
             print('Расшифровка или Зашифровка?')
             action = input()
-            if action == 'Зашифровка':
-                print('Введите слово (с разделом на слога символом "|")')
-                word = input().split('|')
-                last_syllable = word[-1]
-                del word[-1]
-                word.insert(0, last_syllable)
-                print(*word, sep='')
             if action == 'Расшифровка':
-                print('Введите слово (с разделом на слога символом "|")')
-                word = input().split('|')
-                last_syllable = word[0]
-                del word[0]
-                word.append(last_syllable)
-                print(*word, sep='')
+                print('Введите текст')
+                text_encrypted = input()
+                for n in range(len(text_encrypted)):
+                    num_in_secsim = secsim.find(text_encrypted[n])
+                    text_num = digits.find(num_in_secsim)
+                for m in range(len(text_num)):
+                    text_num += (digits[digits.find(text_num[m])])
